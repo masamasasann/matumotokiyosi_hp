28fac8db5fea904cc6ded95664fb5f2ff7723150
github <github>
2021-12-04 09:25:41 +0900

    ファイルアップロード

---
+++ js/index.js
@@ -0,0 +1,214 @@
+// ハンバーガーメニュー
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
+  });
+});
+
+
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 1000,//時間指定(1000＝1秒)
+  strokeWidth: 0.2,//進捗ゲージの太さ
+  color: '#555',//進捗ゲージのカラー
+  trailWidth: 0.2,//ゲージベースの線の太さ
+  trailColor: '#bbb',//ゲージベースの線のカラー
+  text: {//テキストの形状を直接指定       
+    style: {//天地中央に配置
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '-30px 0 0 0',//バーより上に配置
+      transform:'translate(-50%,-50%)',
+      'font-size':'1rem',
+      color: '#fff',
+    },
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function(state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  }
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});  
+
+
+// //スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 200){//上から200pxスクロールしたら
+    $('#page-top').removeClass('RightMove');//#page-topについているRightMoveというクラス名を除く
+    $('#page-top').addClass('LeftMove');//#page-topについているLeftMoveというクラス名を付与
+  }else{
+    if(
+      $('#page-top').hasClass('LeftMove')){//すでに#page-topにLeftMoveというクラス名がついていたら
+      $('#page-top').removeClass('LeftMove');//LeftMoveというクラス名を除き
+      $('#page-top').addClass('RightMove');//RightMoveというクラス名を#page-topに付与
+    }
+  }
+}
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+  $('body,html').animate({
+      scrollTop: 0//ページトップまでスクロール
+  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false;//リンク自体の無効化
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 動きのきっかけとなるアニメーションの名前を定義
+function fadeAnime(){
+
+  // ふわっ
+  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
+    var elemPos = $(this).offset().top-50;//要素より、50px上の
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
+    }else{
+    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
+    }
+    });
+}
+
+
+$('.slider').slick({
+  arrows: false,//左右の矢印はなし
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+  speed: 8000,//スライドのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  slidesToShow: 4,//スライドを画面に4枚見せる
+  slidesToScroll: 1,//1回のスライドで動かす要素数
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+    }
+  }
+]
+});
+
+
+//  順番に現れる
+function delayScrollAnime() {
+  var time = 0.2;//遅延時間を増やす秒数の値
+  var value = time;
+  $('.delayScroll').each(function () {
+    var parent = this;          //親要素を取得
+    var elemPos = $(this).offset().top;//要素の位置まで来たら
+    var scroll = $(window).scrollTop();//スクロール値を取得
+    var windowHeight = $(window).height();//画面の高さを取得
+    var childs = $(this).children();  //子要素を取得
+    
+    if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+      $(childs).each(function () {
+        
+        if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+          
+          $(parent).addClass("play"); //親要素にクラス名playを追加
+          $(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+          $(this).addClass("fadeUp");//アニメーションのクラス名を追加
+          value = value + time;//delay時間を増加させる
+          
+          //全ての処理を終わったらplayを外す
+          var index = $(childs).index(this);
+          if((childs.length-1) == index){
+            $(parent).removeClass("play");
+          }
+        }
+      })
+    }else {
+      $(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+      value = time;//delay初期値の数値に戻す
+    }
+  })
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+  $(window).scroll(function (){
+    delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+// 画面が読み込まれたらすぐに動かしたい場合の記述
+  $(window).on('load', function(){
+    delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+  });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+
+
+// SALE
+$('.slider').slick({
+  arrows: false,//左右の矢印はなし
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 0,//自動的に動き出す待ち時間。初期値は3000ですが今回の見せ方では0
+  speed: 6000,//スライドのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  pauseOnHover: false,//オンマウスでスライドを一時停止させるかどうか。初期値はtrue。
+  pauseOnFocus: false,//フォーカスした際にスライドを一時停止させるかどうか。初期値はtrue。
+  cssEase: 'linear',//動き方。初期値はeaseですが、スムースな動きで見せたいのでlinear
+  slidesToShow: 4,//スライドを画面に4枚見せる
+  slidesToScroll: 1,//1回のスライドで動かす要素数
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1.5,//スライドを画面に1.5枚見せる
+    }
+  }
+]
+});
+
+
