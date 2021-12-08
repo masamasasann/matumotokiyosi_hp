b4604cd67fdc24623b5c2d5869053c7fe19c92af
github <github>
2021-12-09 06:06:13 +0900

    修正

--- js/index.js
+++ js/index.js
@@ -1,3 +1,75 @@
+// ハンバーガーメニュー
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
+  });
+});
+
+
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 1000,//時間指定(1000＝1秒)
+  strokeWidth: 0.2,//進捗ゲージの太さ
+  color: '#555',//進捗ゲージのカラー
+  trailWidth: 0.2,//ゲージベースの線の太さ
+  trailColor: '#bbb',//ゲージベースの線のカラー
+  text: {//テキストの形状を直接指定       
+    style: {//天地中央に配置
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '-30px 0 0 0',//バーより上に配置
+      transform:'translate(-50%,-50%)',
+      'font-size':'1rem',
+      color: '#fff',
+    },
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function(state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  }
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+});  
+
+
+// //スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 200){//上から200pxスクロールしたら
+    $('#page-top').removeClass('RightMove');//#page-topについているRightMoveというクラス名を除く
+    $('#page-top').addClass('LeftMove');//#page-topについているLeftMoveというクラス名を付与
+  }else{
+    if(
+      $('#page-top').hasClass('LeftMove')){//すでに#page-topにLeftMoveというクラス名がついていたら
+      $('#page-top').removeClass('LeftMove');//LeftMoveというクラス名を除き
+      $('#page-top').addClass('RightMove');//RightMoveというクラス名を#page-topに付与
+    }
+  }
+}
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+  $('body,html').animate({
+      scrollTop: 0//ページトップまでスクロール
+  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+  return false;//リンク自体の無効化
+});
+
+
+
 // 動きのきっかけとなるアニメーションの名前を定義
 function fadeAnime(){
 
@@ -118,97 +190,5 @@ $('.slider').not('.slick-initialized').slick({
   }
 ]
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ハンバーガーメニュー
-$(function() {
-  $('.hamburger').click(function() {
-      $(this).toggleClass('active');
-
-      if ($(this).hasClass('active')) {
-          $('.globalMenuSp').addClass('active');
-      } else {
-          $('.globalMenuSp').removeClass('active');
-      }
-  });
-});
-
-
-//テキストのカウントアップ+バーの設定
-var bar = new ProgressBar.Line(splash_text, {//id名を指定
-  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
-  duration: 1000,//時間指定(1000＝1秒)
-  strokeWidth: 0.2,//進捗ゲージの太さ
-  color: '#555',//進捗ゲージのカラー
-  trailWidth: 0.2,//ゲージベースの線の太さ
-  trailColor: '#bbb',//ゲージベースの線のカラー
-  text: {//テキストの形状を直接指定       
-    style: {//天地中央に配置
-      position: 'absolute',
-      left: '50%',
-      top: '50%',
-      padding: '0',
-      margin: '-30px 0 0 0',//バーより上に配置
-      transform:'translate(-50%,-50%)',
-      'font-size':'1rem',
-      color: '#fff',
-    },
-    autoStyleContainer: false //自動付与のスタイルを切る
-  },
-  step: function(state, bar) {
-    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
-  }
-});
-
-//アニメーションスタート
-bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
-  $("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
-});  
-
-
-// //スクロールした際の動きを関数でまとめる
-function PageTopAnime() {
-  var scroll = $(window).scrollTop();
-  if (scroll >= 200){//上から200pxスクロールしたら
-    $('#page-top').removeClass('RightMove');//#page-topについているRightMoveというクラス名を除く
-    $('#page-top').addClass('LeftMove');//#page-topについているLeftMoveというクラス名を付与
-  }else{
-    if(
-      $('#page-top').hasClass('LeftMove')){//すでに#page-topにLeftMoveというクラス名がついていたら
-      $('#page-top').removeClass('LeftMove');//LeftMoveというクラス名を除き
-      $('#page-top').addClass('RightMove');//RightMoveというクラス名を#page-topに付与
-    }
-  }
-}
-
-
-// #page-topをクリックした際の設定
-$('#page-top').click(function () {
-  $('body,html').animate({
-      scrollTop: 0//ページトップまでスクロール
-  }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
-  return false;//リンク自体の無効化
-});
-
 
 
