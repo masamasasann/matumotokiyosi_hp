50e92c7eb5a1bb7281e01a5bf5d220a139488e2d
github <github>
2021-12-14 06:17:46 +0900

    修正

--- js/index.js
+++ js/index.js
@@ -42,22 +42,6 @@ bar.animate(1.0, function () {//バーを描画する割合を指定します 1.
 });  
 
 
-// 動きのきっかけとなるアニメーションの名前を定義
-function fadeAnime(){
-  // ふわっ
-  $('.fadeUpTrigger').each(function(){ //fadeUpTriggerというクラス名が
-    var elemPos = $(this).offset().top-50;//要素より、50px上の
-    var scroll = $(window).scrollTop();
-    var windowHeight = $(window).height();
-    if (scroll >= elemPos - windowHeight){
-    $(this).addClass('fadeUp');// 画面内に入ったらfadeUpというクラス名を追記
-    }else{
-    $(this).removeClass('fadeUp');// 画面外に出たらfadeUpというクラス名を外す
-    }
-    });
-}
-
-
 $('.slider').slick({
   arrows: false,//左右の矢印はなし
   autoplay: true,//自動的に動き出すか。初期値はfalse。
